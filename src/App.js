33f205aab7e6d46f3579f05fc2b0ddcce7c1c933
Your Name <you@example.com>
2024-06-08 17:01:50 +0530

    WeatherApp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,76 @@
+import { useState } from 'react'
+import './App.css'
 
-import './App.css';
+function WeatherApp() {
+    const [search,setSearch] = useState('');
+    const [weatherData,setWeatherData] = useState({})
+    const [loading,setLoading] = useState(false);
 
-function App() {
+    function fetchData(){
+        console.log("fetching Data")
+        setLoading(true)
+        fetch(`https://api.weatherapi.com/v1/current.json?KEY=ce5cd2da50314ca7ac1155357240403&q=${search}`)
+        .then(res => res.json())
+        .then(data => {
+            
+            if(data.error)
+            {
+                setWeatherData(false)
+                throw new Error("Failed to Fetch weather Data")
+            }else {
+                setWeatherData(data)
+            }
+            
+        })
+        .then(() => setLoading(false))
+        .catch(() => {
+            setLoading(false)
+            alert("Failed to fetch weather data")
+        })
+    }
+
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetchData()
+    }
+    const handleChange = (e) => {
+        setSearch(e.target.value)
+    }
+
   return (
-    <div className="App">
-  
-    </div>
-  );
+    <>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <input type="text" placeholder='Enter City Name' onChange={handleChange} value={search}/>
+                <button type='submit'>Search</button>
+            </div>
+        </form>
+
+        {loading && <p className="loading">
+            Loading data...
+        </p>}
+
+        {weatherData && !loading && <div className="weather-cards">
+            <div className="weather-card">
+                <h2>Temperature</h2>
+                <span>{weatherData?.current?.temp_c} C</span>
+            </div>
+            <div className="weather-card">
+                <h2>Humidity</h2>
+                <span>{weatherData?.current?.humidity} C</span>
+            </div>
+            <div className="weather-card">
+                <h2>Condition</h2>
+                <span>{weatherData?.current?.condition?.text} C</span>
+            </div>
+            <div className="weather-card">
+                <h2>Wind Speed</h2>
+                <span>{weatherData?.current?.wind_kph} kph</span>
+            </div>
+        </div>}
+    </>
+  )
 }
 
-export default App;
+export default WeatherApp;
